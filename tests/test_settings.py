31da5ece789decdb0bf071826e8d2845f84942d8
PawKmi <p.t.kmiecik@gmail.com>
2026-04-23 20:42:36 +0200

    Poprawiony commit

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,7 +2,7 @@ from settings import Settings
 
 
 def test_settings_load():
-    # Sprawdzamy czy Settings wczytuje dane (ładowane przez pytest-dotenv)
+    # Sprawdzamy czy Settings wczytuje dane
     settings = Settings()
     assert settings.ENVIRONMENT == "test"
     assert settings.APP_NAME == "Aplikacja-Testowa"
